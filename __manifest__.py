fdce59d5b493d6afcd300c4b005a054d08fa0afb
Luan Pham <phluan92@gmail.com>
2023-05-15 14:15:57 +0000

    finish chapter 7

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real Estate',
+    'summary': 'Practicing developing Odoo modules',
+    'description': "",
+    'website': 'https://www.odoo.com/page/crm',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/estate_property_type_views.xml',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'installable': True,
+    'application': True
+}
---
+++ models/estate_property.py
@@ -0,0 +1,46 @@
+from odoo import fields, models
+from odoo.tools import date_utils
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real estate property"
+
+    name = fields.Char('Title', required=True, translate=True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Date Availability', copy=False, default=lambda self: date_utils.add(fields.Date.today(), months=3))
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living Area (sqm)')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area (sqm)')
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ],
+        help="Orientation can be choosen among North, South, East and West",
+    )
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string='Status',
+        required=True,
+        copy=False,
+        default='new',
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+
+        ]
+    )
+    property_type_id = fields.Many2one("estate.property.type", string="Type")
+    buyer = fields.Man
