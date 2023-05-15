7c8bcb7a977904efd209a9b2a1b3e196a80ff9c6
Luan Pham <phluan92@gmail.com>
2023-05-15 16:11:37 +0000

    finish chapter 8

--- models/estate_property.py
+++ models/estate_property.py
@@ -43,4 +43,9 @@ class EstateProperty(models.Model):
         ]
     )
     property_type_id = fields.Many2one("estate.property.type", string="Type")
-    buyer = fields.Man
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    seller_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
