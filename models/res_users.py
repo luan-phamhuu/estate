356d7462a24c43f4e715ec8ff1f1c699fbe7de94
Luan Pham <phluan92@gmail.com>
2023-05-19 15:07:41 +0000

    finish chapter 13

--- models/res_users.py
+++ models/res_users.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class ResUser(models.Model):
     _inherit = 'res.users'
 
-    property_ids = fields.One2many('estate.property', 'seller_id', string='Properties')
+    property_ids = fields.One2many('estate.property', 'seller_id', string='Properties', domain=[('state', '!=', 'sold')])
